ccf31230cdaa136883e40368acf40e3dcb1861b3
Kevin Hu <adm@Kevins-MacBook-Pro-2.local>
2020-11-02 14:22:25 -0600

    test: uses jsdom to test compose output
    because we need to ensure our output is as expected

--- server/compose.test.js
+++ server/compose.test.js
@@ -1,8 +1,17 @@
 const test = require("tape");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 const compose = require("./compose");
 
 test("compose", async (t) => {
-  t.plan(1);
+  t.plan(2);
   const result = await compose();
   t.equal(typeof result, "string", "compose returns a string");
+  console.log("compose result -->", result);
+  const dom = new JSDOM(result);
+  t.equal(
+    dom.window.document.getElementById("edit").children.length,
+    1,
+    "compose contains a div with id='edit' and exactly 1 child node"
+  );
 });
