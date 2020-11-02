6af75bbde0d7f3df22169c55c143db4245a8af5e
Kevin Hu <adm@Kevins-MacBook-Pro-2.local>
2020-11-02 15:37:10 -0600

    test: fail test to use an SVG icon in pulltab

--- server/compose.test.js
+++ server/compose.test.js
@@ -4,14 +4,18 @@ const { JSDOM } = jsdom;
 const compose = require("./compose");
 
 test("compose", async (t) => {
-  t.plan(2);
+  t.plan(3);
   const result = await compose();
   t.equal(typeof result, "string", "compose returns a string");
-  console.log("compose result -->", result);
   const dom = new JSDOM(result);
   t.equal(
     dom.window.document.getElementById("edit").children.length,
     1,
-    "compose contains a div with id='edit' and exactly 1 child node"
+    "compose contains a div with id='edit' with exactly 1 child node"
+  );
+  t.equal(
+    dom.window.document.getElementById("pulltab").children.item(0).tagName,
+    "SVG",
+    "compose contains a div with id='pulltab' with a first child that is an svg"
   );
 });
