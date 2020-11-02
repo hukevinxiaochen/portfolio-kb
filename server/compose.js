ded0111d73472ffd24e003bdf7aa3669b883b562
Kevin Hu <adm@Kevins-MacBook-Pro-2.local>
2020-11-02 13:18:42 -0600

    feat: compose returns a string

--- server/compose.js
+++ server/compose.js
@@ -1,15 +1,14 @@
 const feather = require("feather-icons");
 const fs = require("fs/promises");
-
+const path = require("path");
 const editIcon = feather.icons["edit-3"].toSvg({ width: 24, height: 24 });
 
 const compose = async () => {
   try {
-    let page = await fs.readFile("./index.html");
-    scaffold.replace(
-      /<div id="edit"><\/div>/,
-      `<div id="edit">${editIcon}</div>`
-    );
+    let page = await fs.readFile(path.resolve(__dirname, "index.html"), {
+      encoding: "utf8",
+    });
+    page.replace(/<div id="edit"><\/div>/, `<div id="edit">${editIcon}</div>`);
     return page;
   } catch (err) {
     console.log(err.message);
--- server/compose.test.js
+++ server/compose.test.js
@@ -1,1 +1,8 @@
-console.log("I'm here")
+const test = require("tape");
+const compose = require("./compose");
+
+test("compose", async (t) => {
+  t.plan(1);
+  const result = await compose();
+  t.equal(typeof result, "string", "compose returns a string");
+});
