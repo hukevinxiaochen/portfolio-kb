c98fad9a06d884ad70166e19362c20bae5fbca84
Kevin Hu <adm@Kevins-MacBook-Pro-2.local>
2020-11-03 05:43:54 -0600

    test: alter tests for edit element
    also add some spacing to test file for readability

--- server/compose.test.js
+++ server/compose.test.js
@@ -6,13 +6,19 @@ const compose = require("./compose");
 test("compose", (t) => {
   t.plan(3);
   const result = compose();
+
+  // 1
   t.equal(typeof result, "string", "compose returns a string");
+
+  // 2
   const dom = new JSDOM(result);
   t.equal(
-    dom.window.document.getElementById("edit").children.length,
-    1,
-    "compose contains a div with id='edit' with exactly 1 child node"
+    dom.window.document.getElementById("edit").tagName.toUpperCase(),
+    "SVG",
+    "compose result contains an svg with id='edit'"
   );
+
+  // 3
   const pullTabChildElementType = dom.window.document
     .getElementById("pulltab")
     .children.item(0)
@@ -20,6 +26,6 @@ test("compose", (t) => {
   t.equal(
     pullTabChildElementType,
     "SVG",
-    "compose contains a div with id='pulltab' with a first child that is an svg"
+    "compose result contains a div with id='pulltab' with a first child that is an svg"
   );
 });
