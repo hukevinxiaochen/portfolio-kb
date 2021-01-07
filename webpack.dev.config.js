c47826e01710a004d459ba09afb0f6e2bce0e051
Kevin <hukxchen@gmail.com>
2021-01-07 17:01:36 -0600

    feat: config webpack to use babel-loader
    and source maps

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,9 +1,30 @@
 const path = require("path");
 
-module.exports = {
-  entry: "./client/assets/js/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+module.exports = (env = { NODE_ENV: "development" }) => {
+  return {
+    entry: "./client/assets/js/index.js",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+  };
 };
